cb88730eec44ae159918fc019c228f57cbf1fc0c
aajwy <13051180828@163.com>
2021-06-24 07:50:29 +0000

    code format
    Signed-off-by: aajwy <13051180828@163.com>

--- services/hilogtool/log_controller.cpp
+++ services/hilogtool/log_controller.cpp
@@ -38,7 +38,7 @@ using namespace std;
 const int MSG_MAX_LEN = 2048;
 const int LOG_PERSIST_FILE_SIZE = 4 * ONE_MB;
 const int LOG_PERSIST_FILE_NUM = 10;
-
+const uint32_t DEFAULT_JOBID = 1;
 void SetMsgHead(MessageHeader* msgHeader, const uint8_t msgCmd, const uint16_t msgLen)
 {
     if (!msgHeader) {
