9b36e6dfc323156f635005f16dacdde4b2863ebb
aajwy <13051180828@163.com>
2021-08-02 13:27:26 +0000

    get pid from socket:ucred
    Signed-off-by: aajwy <13051180828@163.com>

--- services/hilogd/log_collector.cpp
+++ services/hilogd/log_collector.cpp
@@ -26,6 +26,34 @@ namespace OHOS {
 namespace HiviewDFX {
 using namespace std;
 HilogBuffer* LogCollector::hilogBuffer = nullptr;
+int LogCollector::FlowCtrlDataRecv(HilogMsg *msg, int ret)
+{
+    string dropLog = to_string(ret) + " line(s) dropped!";
+    char tag[] = "LOGLIMITD";
+    int len = sizeof(HilogMsg) + sizeof(tag) + 1 + dropLog.size() + 1;
+    HilogMsg *dropMsg = (HilogMsg*)malloc(len);
+    if (dropMsg != nullptr) {
+        dropMsg->len = len;
+        dropMsg->version = msg->version;
+        dropMsg->type = msg->type;
+        dropMsg->level = msg->level;
+        dropMsg->tag_len = sizeof(tag) + 1;
+        dropMsg->tv_sec = msg->tv_sec;
+        dropMsg->tv_nsec = msg->tv_nsec;
+        dropMsg->pid = msg->pid;
+        dropMsg->tid = msg->tid;
+        dropMsg->domain = msg->domain;
+        if (memcpy_s(dropMsg->tag, len - sizeof(HilogMsg), tag, sizeof(tag) + 1)) {
+        }
+        if (memcpy_s(dropMsg->tag + sizeof(tag) + 1, len - sizeof(HilogMsg) - sizeof(tag) - 1,
+            dropLog.c_str(), dropLog.size() + 1)) {
+        }
+        InsertLogToBuffer(*dropMsg);
+        free(dropMsg);
+    }
+    return 0;
+}
+#ifndef __RECV_MSG_WITH_UCRED_
 int LogCollector::onDataRecv(char *data, unsigned int dataLen)
 {
     HilogMsg *msg = (HilogMsg *)data;
@@ -34,34 +62,27 @@ int LogCollector::onDataRecv(char *data, unsigned int dataLen)
     if (ret < 0) {
         return 0;
     } else if (ret > 0) { /* if >0 !Need  print how many lines was dopped */
-        string dropLog = to_string(ret) + " line(s) dropped!";
-        char tag[] = "LOGLIMITD";
-        int len = sizeof(HilogMsg) + sizeof(tag) + 1 + dropLog.size() + 1;
-        HilogMsg *dropMsg = (HilogMsg*)malloc(len);
-        if (dropMsg != nullptr) {
-            dropMsg->len = len;
-            dropMsg->version = msg->version;
-            dropMsg->type = msg->type;
-            dropMsg->level = msg->level;
-            dropMsg->tag_len = sizeof(tag) + 1;
-            dropMsg->tv_sec = msg->tv_sec;
-            dropMsg->tv_nsec = msg->tv_nsec;
-            dropMsg->pid = msg->pid;
-            dropMsg->tid = msg->tid;
-            dropMsg->domain = msg->domain;
-            if (memcpy_s(dropMsg->tag, len - sizeof(HilogMsg), tag, sizeof(tag) + 1)) {
-            }
-            if (memcpy_s(dropMsg->tag + sizeof(tag) + 1, len - sizeof(HilogMsg) - sizeof(tag) - 1,
-                dropLog.c_str(), dropLog.size() + 1)) {
-            }
-            InsertLogToBuffer(*dropMsg);
-            free(dropMsg);
-        }
+        FlowCtrlDataRecv(msg, ret);
     }
-
     InsertLogToBuffer(*msg);
     return 0;
 }
+#else
+int LogCollector::onDataRecv(ucred cred, char *data, unsigned int dataLen)
+{
+    HilogMsg *msg = (HilogMsg *)data;
+    msg->pid = cred.pid;
+    /* Domain flow control */
+    int ret = FlowCtrlDomain(msg);
+    if (ret < 0) {
+        return 0;
+    } else if (ret > 0) { /* if >0 !Need  print how many lines was dopped */
+        FlowCtrlDataRecv(msg, ret);
+    }
+    InsertLogToBuffer(*msg);
+    return 0;
+}
+#endif
 
 LogCollector::LogCollector(HilogBuffer* buffer)
 {
