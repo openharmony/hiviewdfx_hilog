6ac5795343d1020817a4eda0c933ec665581ead6
buzhenwang <buzhenwang@huawei.com>
2022-12-13 21:34:54 +0800

    hilog js api adapter to HOS
    Signed-off-by: buzhenwang <buzhenwang@huawei.com>

--- interfaces/js/kits/napi/src/hilog/src/hilog_napi_base.cpp
+++ interfaces/js/kits/napi/src/hilog/src/hilog_napi_base.cpp
@@ -51,7 +51,7 @@ void ParseLogContent(string& formatStr, vector<napiParam>& params, string& logCo
     uint32_t pos = 0;
     uint32_t count = 0;
     bool debug = true;
-#if not (defined(__WINDOWS__) || defined(__MAC__) || defined(__LINUX__))
+#if not (defined(__WINDOWS__) || defined(__MAC__) || defined(__LINUX__) || defined(HILOG_OHCORE_VERSION))
     debug = IsDebugOn();
 #endif
 
