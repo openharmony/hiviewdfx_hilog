25e54e053f8af7e794605e0a4ed0cbdfb7505f27
liuyifei <liuyifei20@huawei.com>
2024-08-20 06:10:25 +0000

    update frameworks/libhilog/vsnprintf/include/vsprintf_p.h.
    Signed-off-by: liuyifei <liuyifei20@huawei.com>

--- frameworks/libhilog/vsnprintf/include/vsprintf_p.h
+++ frameworks/libhilog/vsnprintf/include/vsprintf_p.h
@@ -19,6 +19,6 @@
 #include <stddef.h>
 #include <stdarg.h>
 
-int vsprintf_p(char *strDest, size_t destMax, const char *format, va_list argList);
+int VsprintfP(char *strDest, size_t destMax, const char *format, va_list argList);
  
 #endif /* HILOG_OVERRIDE_VSPRINTF_P_H */
