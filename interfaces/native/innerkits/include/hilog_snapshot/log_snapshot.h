76fe362fe0d120b1b96a1c6b7ea7f9f3e828cb8b
buzhenwang <buzhenwang@huawei.com>
2025-12-12 11:00:20 +0800

    add libhilog_snapshot
    Signed-off-by: buzhenwang <buzhenwang@huawei.com>

---
+++ interfaces/native/innerkits/include/hilog_snapshot/log_snapshot.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HIVIEWDFX_HILOG_SNAPSHOT_C_H
+#define HIVIEWDFX_HILOG_SNAPSHOT_C_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int HiLogRecordSnapshot(int lines, int64_t time);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // HIVIEWDFX_HILOG_SNAPSHOT_C_H
